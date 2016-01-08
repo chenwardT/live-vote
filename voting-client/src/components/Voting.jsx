53efe92e52f8fffa91bd5a02ff1172c17ec181c9
Edward Chen <chenward.T@gmail.com>
2016-01-08 15:05:57 -0500

    Add index, components: Voting, Vote, Winner, and spec.

---
+++ voting-client/src/components/Voting.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
+
+export default React.createClass({
+  getPair: function() {
+    return this.props.pair || [];
+  },
+  isDisabled: function() {
+    return !!this.props.hasVoted;
+  },
+  hasVotedFor: function(entry) {
+    return this.props.hasVoted === entry;
+  },
+  render: function() {
+    return <div className="voting">
+      {this.props.winner ?
+        <Winner ref="winner" winner={this.props.winner} /> :
+        <Vote {...this.props} />}
+    </div>;
+  }
+});
